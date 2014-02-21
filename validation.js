e99dab57eb93749e913a3b757db761955b47b5ad
shiv shakti <shivshakti.prasad@gmail.com>
2014-02-21 20:31:13 +0530

    type property added
    A type property is added that makes replacement for url, alphanumeric,
    number, email etc properties which is now passed as string of object
    values to type. When string: it just mentions the type like 'url',
    'alphanumeric', 'email'. When object must contain the following
    properties: value : "alphanumeric", errMessage: "hello error!!".

--- validation.js
+++ validation.js
@@ -22,11 +22,11 @@
         return output;
     };
 }(jQuery));
-function OverlayMessage(control, isValid, message) {
-    if (isValid) {
-        $(control).prop("borderRestore", control.style.borderColor).css({ "border-color": "" });
+function OverlayMessage(result) {
+    if (result.isValid) {
+        $(result.control).prop("borderRestore", result.control.style.borderColor).css({ "border-color": "" });
     } else {
-        $(control).prop("borderRestore", control.style.borderColor).css({ "border-color": "red" });
+        $(result.control).prop("borderRestore", result.control.style.borderColor).css({ "border-color": "red" });
     }
 }
 
@@ -35,23 +35,10 @@ function validate(opts) {
     var options = $(Element).attr("validate");
     if (options) {
         options = eval('options={' + options + '}');
-        $.extend(options, { value: $(Element).val() });
+        $.extend(options, { value: $(Element).val(), caption: $(Element).val()});
         var res = Validation.validate(options);
         $.extend(res, { control: Element });
-        if (!res.isValid) {
-            var caption = $(Element).attr("caption");
-            if (caption) {
-                res.message = caption + " is " + res.message;
-            }
-            //if (options.overlay || (opts && opts.overlay) ) {
-                OverlayMessage(res.control, res.isValid, res.message);
-            //}
-                if ("object" === typeof opts) {
-                    if ('function' === typeof opts.call) {
-                        opts.call.apply(res);
-                    }
-                }
-        }
+        OverlayMessage(res);
         return res;
     }
 }
@@ -71,16 +58,22 @@ Validation = {
             }
         }
     },
+    _callBack: function (type) {
+        var ret = type.value();
+        if (ret === false) {
+            return new this.ValidationResult(false, type.errMessage);
+        }
+    },
     validate: function (options) {
         var isEmpty = false;
         //validate for mandatory
-        if (options.mandatory) {
+        if (options.required) {
             if (options.value == undefined)
-                return new this.ValidationResult(false, "mandatory");
+                return new this.ValidationResult(false, "required");
             if ('object' == typeof options.value)
-                return new this.ValidationResult(false, "mandatory");
+                return new this.ValidationResult(false, "required");
             if ('string' == typeof options.value && options.value == "")
-                return new this.ValidationResult(false, "mandatory");
+                return new this.ValidationResult(false, "required");
         }else{
             if (options.value == undefined)
                 isEmpty = true;
@@ -88,19 +81,6 @@ Validation = {
                 isEmpty = true;
             if ('string' == typeof options.value && options.value == "")
                 isEmpty = true;
-        }
-        var allowedChars = false;
-        if (options.allowedChar) {
-            if ('string' === typeof options.allowedChar) {
-                allowedChars = true;
-            }
-        }
-
-        //validate for number
-        if (options.number) {
-            if (isNaN(parseFloat(options.value)) && !isFinite(options.value)) {
-                return new this.ValidationResult(false, "not a number");
-            }
         }
 
         //validate for minimum length of string
@@ -117,59 +97,117 @@ Validation = {
             }
         }
 
-        //validate with the explicit regular expression
-        if (options.regexp && !isEmpty) {
-            if (!options.regexp.test(options.value)) {
-                return new this.ValidationResult(false, "incorrect");
-            }
-        }
-
-        //validate for alphanumeric characters
-        if (options.alphanumeric && !isEmpty) {
-            if (!(/^[a-z0-9]+$/i.test(options.value))) {
-                return new this.ValidationResult(false, "not alphanumeric");
-            }
-        }
-
-        //validate for email
-        if (options.email && !isEmpty) {
-            if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(options.value))) {
-                return new this.ValidationResult(false, "not an email");
-            }
-        }
-
-        //validate for url
-        if (options.url && !isEmpty) {
-            if (!(/^(ftp:\/\/|http:\/\/|https:\/\/)?(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!-\/]))?$/.test(options.value))) {
-                return new this.ValidationResult(false, "not a url");
-            }
-        }
-
         //validate the value in a valid list
-        if (options.in) {
+        if (options.allow) {
             var valid = false;
-            for (var ele in options.in) {
+            for (var ele in options.allow) {
                 if (ele == options.value) {
                     valid = true;
                     break;
                 }
             }
             if (!valid) {
-                return new this.ValidationResult(false, "not in the valid list");
+                return new this.ValidationResult(false, options.errMessage ? options.errMessage : options.caption + " is not in the valid list");
+            } else {
+                return new this.ValidationResult(true);
             }
         }
 
         //validate the valuse not in the invalid list
-        if (options.notIn) {
+        if (options.notAllow) {
             var valid = true;
-            for (var ele in options.notIn) {
+            for (var ele in options.notAllow) {
                 if (ele == options.value) {
                     valid = false;
                     break;
                 }
             }
             if (!valid) {
-                return new this.ValidationResult(false, "in the invalid list");
+                return new this.ValidationResult(false, options.errMessage ? options.errMessage : options.caption + " is in the invalid list");
+            }
+        }
+
+        var type = {};
+        if ('string' === typeof options.type) {
+            type.value = options.type;
+            type.errMessage = null;
+        } else if (('object' === typeof options.type) && (options.type !== null)) {
+            if ('function' === typeof options.type.value) {
+                type.value = options.type.value;
+                type.errMessage = options.type.errMessage ? options.type.errMessage : options.errMessage ? options.errMessage : options.caption + " is invalid";
+                type.expr = null;
+                return this._callBack(type);
+            } else {
+                type.value = options.type.value ? options.type.value : null;
+                type.errMessage = options.type.errMessage ? options.type.errMessage : options.errMessage ? options.errMessage : null;
+                type.expr = options.type.expr ? options.type.expr : null;
+                type.allowContent = options.type.allowContent ? options.type.allowContent : null;
+                if (options.type.notAllowContent) {
+                    if ('string' === typeof options.type.notAllowContent) {
+                        if (options.value.contains(options.type.notAllowContent)) {
+                            return new this.ValidationResult(false, type.errMessage ? type.errMessage : options.caption + "some characters of " + options.caption + "are not allowd");
+                        }
+                    }else if ('object' === typeof options.type.notAllowContent) {
+                        for (var i = 0; i < options.type.notAllowContent.length; i++) {
+                            if (options.value.contains(options.type.notAllowContent[i])) {
+                                return new this.ValidationResult(false, type.errMessage ? type.errMessage : options.caption + "some characters of " + options.caption + "are not allowd");
+                            }
+                        }
+                    }
+                }
+                type.IsStartWithAlpha = options.type.IsStartWithAlpha ? true : false;
+            }
+        } else if ('function' === typeof options.type) {
+            type.value = options.type;
+            type.errMessage = options.errMessage ? options.errMessage : options.caption + " is invalid";
+            type.expr = null;
+            return this._callBack(type);
+        } else {
+            type.value = null;
+            type.errMessage = null;
+            type.expr = null;
+        }
+
+        //validate for number
+        if (type.value === 'number') {
+            if (isNaN(parseFloat(options.value)) && !isFinite(options.value)) {
+                return new this.ValidationResult(false, type.errMessage ? type.errMessage : options.caption + " is not a number");
+            }
+        }
+
+        //validate with the explicit regular expression
+        if (type.value === 'regexp' && type.expr && !isEmpty) {
+            if (!type.expr.test(options.value)) {
+                return new this.ValidationResult(false, type.errMessage ? type.errMessage : options.caption + " is incorrect");
+            }
+        }
+
+        //validate for alphanumeric characters
+        if (type.value === 'alphanumeric' && !isEmpty) {
+            var val = options.value;
+            if (type.allowContent) {
+                for (var i = 0; i < type.allowContent.length; i++) {
+                    while (val.contains(type.allowContent[i])) {
+                        val = val.replace(type.allowContent[i], "");
+                    }
+                }
+            }
+            if (!(/^[a-z0-9]+$/i.test(val))) {
+                return new this.ValidationResult(false, type.errMessage ? type.errMessage : options.caption + " is not alphanumeric");
+            }
+        }
+
+        //validate for email
+        if (type.value === 'email' && !isEmpty) {
+            if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(options.value))) {
+                return new this.ValidationResult(false, type.errMessage ? type.errMessage : options.caption + " is not an email");
+            }
+        }
+
+        //validate for url
+        if (type.value === 'url' && !isEmpty) {
+            if (!(/^(ftp:\/\/|http:\/\/|https:\/\/)?(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!-\/]))?$/.test(options.value))) {
+                return new this.ValidationResult(false, type.errMessage ? type.errMessage : options.caption + " is not a url");
             }
         }
 
